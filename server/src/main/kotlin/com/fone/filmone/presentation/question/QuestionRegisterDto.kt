cd20fa899ba5dbd6412dc42f4048301930e505c5
sinkyoungdeok <tkagmd1@naver.com>
2022-12-28 02:08:36 +0900

    [#26] question register api

--- server/src/main/kotlin/com/fone/filmone/presentation/question/QuestionRegisterDto.kt
+++ server/src/main/kotlin/com/fone/filmone/presentation/question/QuestionRegisterDto.kt
@@ -1,5 +1,6 @@
 package com.fone.filmone.presentation.question
 
+import com.fone.filmone.domain.question.entity.Question
 import com.fone.filmone.domain.question.enum.Type
 
 class QuestionRegisterDto {
@@ -10,7 +11,19 @@ class QuestionRegisterDto {
         val title: String,
         val description: String,
         val agreeToPersonalInformation: Boolean,
-    )
+    ) {
+
+        fun toEntity(): Question {
+
+            return Question(
+                email = email,
+                type = type,
+                title = title,
+                description = description,
+                agreeToPersonalInformation = agreeToPersonalInformation,
+            )
+        }
+    }
 
     data class QuestionRegisterResponse(
         val email: String,
@@ -18,5 +31,16 @@ class QuestionRegisterDto {
         val title: String,
         val description: String,
         val agreeToPersonalInformation: Boolean,
-    )
+    ) {
+
+        constructor(
+            question: Question
+        ) : this(
+            email = question.email,
+            type = question.type,
+            title = question.title,
+            description = question.description,
+            agreeToPersonalInformation = question.agreeToPersonalInformation,
+        )
+    }
 }
