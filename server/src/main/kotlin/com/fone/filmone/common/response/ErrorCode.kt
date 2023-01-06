a0bff81bca1cee837e6e18b94d422b9671a700e8
sinkyoungdeok <tkagmd1@naver.com>
2023-01-06 23:12:14 +0900

    [feat-38] login api 스펙 변경

--- server/src/main/kotlin/com/fone/filmone/common/response/ErrorCode.kt
+++ server/src/main/kotlin/com/fone/filmone/common/response/ErrorCode.kt
@@ -3,7 +3,11 @@ package com.fone.filmone.common.response
 enum class ErrorCode(
     val errorMsg: String,
 ) {
-    COMMON_SYSTEM_ERROR("일시적인 오류가 발생했습니다. 잠시 후 다시 시도해주세요.")
+    // 500
+    COMMON_SYSTEM_ERROR("일시적인 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+
+    // 400
+    COMMON_INVALID_PARAMETER("요청한 값이 올바르지 않습니다."),
     ;
 
     fun getErrorMsg(vararg arg: Any?): String {
