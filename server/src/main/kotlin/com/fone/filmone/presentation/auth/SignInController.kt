2c75878f8428e850a4110688a945ad5cd250cc2f
sinkyoungdeok <tkagmd1@naver.com>
2023-01-12 00:36:55 +0900

    [feat-55] auth -> user api url

--- server/src/main/kotlin/com/fone/filmone/presentation/auth/SignInController.kt
+++ server/src/main/kotlin/com/fone/filmone/presentation/auth/SignInController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api/v1/user")
 class SignInController(
     private val signInFacade: SignInFacade,
 ) {
--- server/src/main/kotlin/com/fone/filmone/presentation/auth/SignUpController.kt
+++ server/src/main/kotlin/com/fone/filmone/presentation/auth/SignUpController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/user")
 class SignUpController(
     private val signUpFacade: SignUpFacade,
 ) {
